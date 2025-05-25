3804a95f600b9d6cc175dca184344bc4597a4a5d
54weasels <54weasels@gmail.com>
2025-05-24 23:35:00 -0700

    Add the PAL images and other relevant softwre

---
+++ pal/pal2gal.bat
@@ -0,0 +1,34 @@
+pal2gal.exe -d PAL16R4 -g GAL16V8   -e errors -o 1564-02.gdj 1564-02.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1565-01.gdj 1565-01.jed
+pal2gal.exe -d PAL16R4 -g GAL16V8   -e errors -o 1566-02.gdj 1566-02.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1567-01.gdj 1567-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1568-01.gdj 1568-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1569-01.gdj 1569-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1570-01.gdj 1570-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1571-01.gdj 1571-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1572-02.gdj 1572-02.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1573-01.gdj 1573-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1574-01.gdj 1574-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1575-01.gdj 1575-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1576-01.gdj 1576-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1577-01.gdj 1577-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1578-01.gdj 1578-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1579-02.gdj 1579-02.jed
+pal2gal.exe -d PAL16R6 -g GAL16V8   -e errors -o 1580-01.gdj 1580-01.jed
+pal2gal.exe -d PAL20L8 -g GAL22V10  -e errors -o 1581-01.gdj 1581-01.jed
+pal2gal.exe -d PAL20L8 -g GAL22V10  -e errors -o 1582-02.gdj 1582-02.jed
+pal2gal.exe -d PAL20L8 -g GAL22V10  -e errors -o 1583-02.gdj 1583-02.jed
+pal2gal.exe -d PAL20L8 -g GAL22V10  -e errors -o 1584-01.gdj 1584-01.jed
+pal2gal.exe -d PAL20L8 -g GAL22V10  -e errors -o 1585-01.gdj 1585-01.jed
+pal2gal.exe -d PAL20L8 -g GAL22V10  -e errors -o 1586-02.gdj 1586-02.jed
+pal2gal.exe -d PAL20R4 -g GAL22V10  -e errors -o 1587-01.gdj 1587-01.jed
+pal2gal.exe -d PAL20R6 -g GAL22V10  -e errors -o 1588-01.gdj 1588-01.jed
+REM pal2gal.exe -d PAL8L14 -g  -e errors 1589-01.jed
+pal2gal.exe -d PAL16L8 -g GAL16V8   -e errors -o 1590-01.gdj 1590-01.jed
+pal2gal.exe -d PAL16R8 -g GAL16V8   -e errors -o 1594-01.gdj 1594-01.jed
+REM pal2gal.exe -d PAL20X8 -g GAL20XV10 -e errors -o 1595-01.gdj 1595-01.jed
+REM pal2gal.exe -d PAL20X8 -g GAL20XV10 -e errors -o 1596-01.gdj 1596-01.jed
+REM pal2gal.exe -d PAL20X8 -g GAL20XV10 -e errors -o 1597-01.gdj 1597-01.jed
+REM pal2gal.exe -d PAL20X8 -g GAL20XV10 -e errors -o 1598-01.gdj 1598-01.jed
+pal2gal.exe -d PAL16R4 -g GAL16V8   -e errors -o 1599-01.gdj 1599-01.jed
+pal2gal.exe -d PAL20L8 -g GAL22V10  -e errors -o 1683-03.gdj 1683-03.jed
